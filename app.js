f38d5ee37816d2f4861b0f5eed6df590a9d72c2c
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2024-02-21 21:16:33 +0200

    Add rest of units

--- app.js
+++ app.js
@@ -1,98 +1,149 @@
-'use strict';
+"use strict";
 
-const DARK_COLOUR = 'rgb(51, 53, 64)';
-const WHITE_COLOUR = '#f6f6b8';
+const DARK_COLOUR = "rgb(51, 53, 64)";
+const WHITE_COLOUR = "#f6f6b8";
+const HEADER_LIGHT = "#ebeb6d";
+const HEADER_DARK = "#c7c7c7";
 
-let theme = 'light';
+let theme = "light";
 
-$("#themeBtn").click(function(){
-  if (theme === 'dark') {
-    $(':root').css('--bg-color', WHITE_COLOUR);
-    $(':root').css('--text-color', DARK_COLOUR);
-    $('#themeBtn').toggleClass('fa-sun');
-    $('#themeBtn').toggleClass('fa-moon');
-    theme = 'light';
+$("#themeBtn").click(function () {
+  if (theme === "dark") {
+    $(":root").css("--bg-color", WHITE_COLOUR);
+    $(":root").css("--text-color", DARK_COLOUR);
+    $(":root").css("--header-color", HEADER_LIGHT);
+    $("#themeBtn").toggleClass("fa-sun");
+    $("#themeBtn").toggleClass("fa-moon");
+    theme = "light";
   } else {
-    $(':root').css('--bg-color', DARK_COLOUR);
-    $(':root').css('--text-color', WHITE_COLOUR);
-    $('#themeBtn').toggleClass('fa-sun');
-    $('#themeBtn').toggleClass('fa-moon');
-    theme = 'dark';
+    $(":root").css("--bg-color", DARK_COLOUR);
+    $(":root").css("--text-color", WHITE_COLOUR);
+    $(":root").css("--header-color", HEADER_DARK);
+    $("#themeBtn").toggleClass("fa-sun");
+    $("#themeBtn").toggleClass("fa-moon");
+    theme = "dark";
   }
-})
+});
 
-$("#spearmanInput").on("input", function() {
+$("#spearmanInput").on("input", function () {
   const result = Math.round($(this).val() * 0.6);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
-})
+});
 
-$("#slingerInput").on("input", function() {
+$("#slingerInput").on("input", function () {
   const result = Math.round($(this).val() * 0.4);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
-$("#swordsmanInput").on("input", function() {
+$("#swordsmanInput").on("input", function () {
   const result = Math.round($(this).val() * 1.2);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
-$("#hopliteInput").on("input", function() {
+$("#hopliteInput").on("input", function () {
   const result = Math.round($(this).val() * 1.4);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
-$("#ramInput").on("input", function() {
+$("#ramInput").on("input", function () {
   const result = Math.round($(this).val() * 4.4);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
-$("#archerInput").on("input", function() {
+$("#archerInput").on("input", function () {
   const result = Math.round($(this).val() * 1.1);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
-$("#catapultInput").on("input", function() {
+$("#catapultInput").on("input", function () {
   const result = Math.round($(this).val() * 11.2);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
-$("#carabineerInput").on("input", function() {
+$("#carabineerInput").on("input", function () {
   const result = Math.round($(this).val() * 4);
-  const span = $(this).parent().find('span');
+  const span = $(this).parent().find("span");
   span.text(result);
   calcTotalPoints();
   calcTotalUpkeep();
 });
 
+$("#giantInput").on("input", function () {
+  const result = Math.round($(this).val() * 12.4);
+  const span = $(this).parent().find("span");
+  span.text(result);
+  calcTotalPoints();
+  calcTotalUpkeep();
+});
+
+$("#mortarInput").on("input", function () {
+  const result = Math.round($(this).val() * 62);
+  const span = $(this).parent().find("span");
+  span.text(result);
+  calcTotalPoints();
+  calcTotalUpkeep();
+});
+
+$("#bombardInput").on("input", function () {
+  const result = Math.round($(this).val() * 11.6);
+  const span = $(this).parent().find("span");
+  span.text(result);
+  calcTotalPoints();
+  calcTotalUpkeep();
+});
+
+$("#gyrocopterInput").on("input", function () {
+  const result = Math.round($(this).val() * 2.5);
+  const span = $(this).parent().find("span");
+  span.text(result);
+  calcTotalPoints();
+  calcTotalUpkeep();
+});
+
+$("#cookInput").on("input", function () {
+  const result = Math.round($(this).val() * 4);
+  const span = $(this).parent().find("span");
+  span.text(result);
+  calcTotalPoints();
+  calcTotalUpkeep();
+});
+
+$("#doctorInput").on("input", function () {
+  const result = Math.round($(this).val() * 10);
+  const span = $(this).parent().find("span");
+  span.text(result);
+  calcTotalPoints();
+  calcTotalUpkeep();
+});
 
 function calcTotalPoints() {
   let totalPoints = 0;
 
   // calculate total points in span elements
-  $('.militaryPoints').each(function() {
+  $(".militaryPoints").each(function () {
     totalPoints += parseInt($(this).text() || 0);
-  })
+  });
 
   $("#totalPoints").text(totalPoints);
 }
@@ -100,10 +151,10 @@ function calcTotalPoints() {
 function calcTotalUpkeep() {
   let totalUpkeep = 0;
 
-  $('.militaryPoints').each(function() {
-    const inputId = $(this).parent().prevAll('input').attr('id');
-    const inputValue = parseInt($('#' + inputId).val() || 0);
-    const unitUpkeep = parseInt($(this).data('upkeep') || 0);
+  $(".militaryPoints").each(function () {
+    const inputId = $(this).parent().prevAll("input").attr("id");
+    const inputValue = parseInt($("#" + inputId).val() || 0);
+    const unitUpkeep = parseInt($(this).data("upkeep") || 0);
 
     totalUpkeep += inputValue * unitUpkeep;
   });
@@ -112,5 +163,5 @@ function calcTotalUpkeep() {
 
   // Update the totalUpkeep to the respective container or element
   // For example, assuming there is a container with id "totalUpkeep"
-  $("#totalUpkeep").find('span').text(totalUpkeep);
+  $("#totalUpkeep").find("span").text(totalUpkeep);
 }

89babb3a0f06e7cf746e4f2ad465be0e5fcca7cc
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2024-03-07 00:20:15 +0200

    Add boilerplate for buildings
    Create layout for the buildings and calculating materials needed

--- app.js
+++ app.js
@@ -5,19 +5,26 @@ const WHITE_COLOUR = "#f6f6b8";
 const HEADER_LIGHT = "#ebeb6d";
 const HEADER_DARK = "#c7c7c7";
 
+const BG_COLOUR_LIGHT = "#ebeb6d";
+const BG_COLOUR_DARK = "rgba(0, 0, 0, 0.6)";
+
 let theme = localStorage.theme || "light";
 
 $("document").ready(function () {
   if (theme === "light") {
-    $(":root").css("--bg-color", WHITE_COLOUR);
-    $(":root").css("--text-color", DARK_COLOUR);
-    $(":root").css("--navbar-color", HEADER_LIGHT);
+    $(":root").css("--bg-colour", WHITE_COLOUR);
+    $(":root").css("--bg-colour-dim", BG_COLOUR_LIGHT);
+    $(":root").css("--text-colour", DARK_COLOUR);
+    $(":root").css("--link-colour", DARK_COLOUR);
+    $(":root").css("--navbar-colour", HEADER_LIGHT);
     $("#themeBtn").toggleClass("fa-sun");
     $("#themeBtn").toggleClass("fa-moon");
   } else {
-    $(":root").css("--bg-color", DARK_COLOUR);
-    $(":root").css("--text-color", WHITE_COLOUR);
-    $(":root").css("--navbar-color", HEADER_DARK);
+    $(":root").css("--bg-colour", DARK_COLOUR);
+    $(":root").css("--bg-colour-dim", BG_COLOUR_DARK);
+    $(":root").css("--text-colour", WHITE_COLOUR);
+    $(":root").css("--link-colour", WHITE_COLOUR);
+    $(":root").css("--navbar-colour", HEADER_DARK);
     $("#themeBtn").toggleClass("fa-sun");
     $("#themeBtn").toggleClass("fa-moon");
   }
@@ -25,17 +32,21 @@ $("document").ready(function () {
 
 $("#themeBtn").click(function () {
   if (theme === "dark") {
-    $(":root").css("--bg-color", WHITE_COLOUR);
-    $(":root").css("--text-color", DARK_COLOUR);
-    $(":root").css("--navbar-color", HEADER_LIGHT);
+    $(":root").css("--bg-colour", WHITE_COLOUR);
+    $(":root").css("--bg-colour-dim", BG_COLOUR_LIGHT);
+    $(":root").css("--text-colour", DARK_COLOUR);
+    $(":root").css("--link-colour", DARK_COLOUR);
+    $(":root").css("--navbar-colour", HEADER_LIGHT);
     $("#themeBtn").toggleClass("fa-sun");
     $("#themeBtn").toggleClass("fa-moon");
     theme = "light";
     localStorage.setItem("theme", theme);
   } else {
-    $(":root").css("--bg-color", DARK_COLOUR);
-    $(":root").css("--text-color", WHITE_COLOUR);
-    $(":root").css("--navbar-color", HEADER_DARK);
+    $(":root").css("--bg-colour", DARK_COLOUR);
+    $(":root").css("--bg-colour-dim", BG_COLOUR_DARK);
+    $(":root").css("--text-colour", WHITE_COLOUR);
+    $(":root").css("--link-colour", WHITE_COLOUR);
+    $(":root").css("--navbar-colour", HEADER_DARK);
     $("#themeBtn").toggleClass("fa-sun");
     $("#themeBtn").toggleClass("fa-moon");
     theme = "dark";
